ebf57d66e9531df527c6cec96dee566bc06fe849
ManikyaAlister <m.alister@uq.net.au>
2022-10-27 14:55:32 +1100

    recovery v+a exp bug + parallel

--- Recovery/v-exp/run-parallel/06_runDiffModel_complex_recovery91-100.R
+++ Recovery/v-exp/run-parallel/06_runDiffModel_complex_recovery91-100.R
@@ -5,16 +5,16 @@ source(file = here("Recovery/5.0.0_load-packages.R"))
 source(file = here("Recovery/02_deep-background.R"))
 
 conds= 1 # number of experimental conditions to loop over
-model = "a-exp" 
+model = "v-exp" 
 nSub = 100 # number of subjects to run 
 
 ####################################
 #### Exponential Threshold Model ###
 ####################################
 
-for (useSub in 1:nSub) { # Run DDM for each subject in n Subjects
+for (useSub in 91:nSub) { # Run DDM for each subject in n Subjects
   
-  load(paste("Recovery/",model,"/Datasets/RECOVERY_DATA-DIFF_LHS-",useSub,".Rdata",sep=""))
+  load(paste("Recovery/Datasets/RECOVERY_DATA-DIFF_LHS-",useSub,".Rdata",sep=""))
   newSeed=Sys.time()
   set.seed(as.numeric(newSeed))
   
@@ -23,27 +23,27 @@ for (useSub in 1:nSub) { # Run DDM for each subject in n Subjects
     names(x)=par.names
     
     for (cond in conds) {
-      a=x["a.asym"]+x["a.start"]*exp(-x["a.rate"]*data$Trial)
+      a=x["a"]
       t0=x["t0"]
-      v=x["v"]
-      z=0.5
+      v=(x["v.asym"]+x["v.start"])-x["v.start"]*exp(-x["v.rate"]*data$Trial)
+      z=x["z"]
       sv=0
       sz=0
       st0=0
       s=1
-      tmp=ddiffusion(rt=data$time[data$Cond==cond],response=(3-data$Resp[data$Cond==cond]),z=z*a,a=a,v=v,t0=t0-(st0/2),s=s,sv=sv,sz=sz,st0=st0) #if I want to do it over multiple conditions
+      tmp=ddiffusion(rt=data$time[data$Cond==cond],response=data$Resp[data$Cond==cond],z=z*a,a=a,v=v,t0=t0-(st0/2),s=s,sv=sv,sz=sz,st0=st0) #if I want to do it over multiple conditions
       out=out+sum(log(pmax(tmp,1e-10)))
     }
     out
   }
   
-  theta.names=c("v","t0",
-                "a.start","a.asym","a.rate")
+  theta.names=c("a","t0","z",
+                "v.start","v.asym","v.rate")
   
-  savefile=here(paste("Recovery/",model,"/Fits_recovery/P",useSub,"_",model,".Rdata",sep=""))
+  savefile=here(paste("Recovery/Fits_recovery/P",useSub,"_",model,".Rdata",sep=""))
   #saveIC = here(paste("data/evansetal-18/derived/P",useSub,"_",model,"-IC.Rdata",sep=""))
   
-  source(here("Recovery/03_priors/03.1.3_a-priors-pow-exp.R"))
+  source(here("Recovery/03_priors/03.2.3_v-priors-pow-exp.R"))
   source(here("Recovery/04_iterative-process.R"))
   
   n.pars = length(theta.names)
