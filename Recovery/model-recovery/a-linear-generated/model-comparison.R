20801c97a7aa04c978202b22aa326ee7761bcf5d
ManikyaAlister <alisterm@student.unimelb.edu.au>
2023-10-18 13:30:44 +1100

    fixing save issue with a-linear

--- Recovery/model-recovery/a-linear-generated/model-comparison.R
+++ Recovery/model-recovery/a-linear-generated/model-comparison.R
@@ -5,14 +5,14 @@ IC_array = function(models, criterion, generating, grouping_param, bad_datasets 
   # Set up an empty data frame with named columns for models
   allIC <- data.frame(matrix(ncol = length(models)))
   colnames(allIC) <- models
-  gen_param <- numeric(n)  # Initialize gen_param as a numeric vector
+  gen_param <- numeric(n_sub)  # Initialize gen_param as a numeric vector
   
   for (j in 1:length(models)) {
     model <- models[j]
     gen <- generating[j]
     generating_data <- paste0(models[generating == TRUE], "-generated")
     
-    for (i in 76:n) {
+    for (i in 1:n_sub) {
       if (i %in% bad_datasets) {
         next
       }
@@ -57,15 +57,15 @@ IC_array = function(models, criterion, generating, grouping_param, bad_datasets 
   
   # Clean the data frame by removing rows with NAs
   allIC <- allIC[order(gen_param),]
-  allIC <- allIC[complete.cases(allIC),]
+  #allIC <- allIC[complete.cases(allIC),]
   
   
   return(allIC)  # Return the cleaned data frame
 }
 
-bad_datasets = c(75)
+bad_datasets = c(16)
 
-n = 100
+n_sub= 100
 
 
 recovering_model <- c("a-exp", "a-delayed-exp", "simple")
