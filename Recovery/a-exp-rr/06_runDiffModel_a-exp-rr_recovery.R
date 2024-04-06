19f32a3eb4e3af2b9e7665a4e33addb2d75d3d9e
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-04-06 20:44:09 +1100

    commit for pull

--- Recovery/a-exp-rr/06_runDiffModel_a-exp-rr_recovery.R
+++ Recovery/a-exp-rr/06_runDiffModel_a-exp-rr_recovery.R
@@ -50,7 +50,7 @@ for (useSub in subj) { # Run DDM for each subject in n Subjects
   
   AIC = -2*max(weight)+ 2*n.pars 
   BIC = log(length(data$Time))*n.pars-2*max(weight)
-  #save(AIC,BIC,file = saveIC)
+  save(AIC,BIC,file = saveIC)
   save(AIC, BIC, theta,weight,data,burnin,nmc,n.chains,theta.names,conds, genParams,
        file=savefile)
 }
