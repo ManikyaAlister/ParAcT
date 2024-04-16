b9073be14fa0d316b7a138e028a48e1134ecd5ab
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-04-17 08:34:03 +1000

    potential fix for block iteration models

--- modelling/generic_scripts/likelihood-function.R
+++ modelling/generic_scripts/likelihood-function.R
@@ -54,12 +54,39 @@ log.dens.like.blocked = function (x, data, par.names, functions = paract_functio
     } else{
       stim_time <- data$Trial[data$Stim == stim & data$Block == block]
     }
-    print(paste0("Block ",block))
+      
+    # function to get the arguments of another function
+    funArgs = function(fun) {
+      names(formals(fun))
+    }
+
     # get estimates 
-    a = functions$a(x, time = stim_time)
-    t0 = functions$t0(x, time = stim_time)
-    v = functions$v(x, time = stim_time)
-    z = functions$z(x, time = stim_time, stimulus = stim)
+    
+    # check to see if there is a "block" (b) argument in the function, and if there is, add it. 
+    if ("b" %in% funArgs(functions$a)){
+      a = functions$a(x, time = stim_time, b = block)
+    } else {
+      a = functions$a(x, time = stim_time)
+    } 
+    
+    if ("b" %in% funArgs(functions$v)){
+      v = functions$v(x, time = stim_time, b = block)
+    } else {
+      v = functions$v(x, time = stim_time)
+    }
+    
+    if ("b" %in% funArgs(functions$t0)){
+      t0 = functions$t0(x, time = stim_time, b = block)
+    } else {
+      t0 = functions$t0(x, time = stim_time)
+    }
+    
+    if ("b" %in% funArgs(functions$z)){
+      z = functions$z(x, time = stim_time, stimulus = stim, b = block)
+    } else {
+      z = functions$z(x, time = stim_time, stimulus = stim)
+    }
+    
     sv = 0
     sz = 0
     st0 = 0
