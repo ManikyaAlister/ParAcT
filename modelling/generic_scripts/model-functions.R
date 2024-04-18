85c29c1afe3d30eb237134770bf63ccc886d30c7
ManikyaAlister <alisterm@student.unimelb.edu.au>
2024-04-19 06:35:46 +1000

    2p models to model functions

--- modelling/generic_scripts/model-functions.R
+++ modelling/generic_scripts/model-functions.R
@@ -72,6 +72,9 @@ all_functions <- list(
     t0 = t0_standard,
     blocked_likelihood = FALSE
   ),
+
+# Block-varying models ----------------------------------------------------
+
   "v-linear-blocked" = list(
     a = a_standard,
     v = v_linear_blocked,
@@ -107,6 +110,9 @@ all_functions <- list(
     t0 = t0_standard,
     blocked_likelihood = FALSE
   ),
+
+# Blocked likelihood models -----------------------------------------------
+
   "a-step-fixed" = list(
     a = a_step_fixed,
     v = v_standard,
@@ -145,6 +151,88 @@ all_functions <- list(
   "v-blocked-complex" = list(
     a = a_standard, 
     v = v_blocked_complex, 
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = TRUE
+  ),
+
+# Two parameter models ----------------------------------------------------
+# 2 param models to include: 
+#      [,1]                               
+# [1,] "a-step-fixed+v-step-fixed"        
+# [2,] "a-exp+v-exp"                      
+# [3,] "a-exp+v-linear-blocked"           
+# [4,] "a-exp+v-step-fixed"               
+# [5,] "a-dExp-blocked+v-linear-blocked"  
+# [6,] "a-exp-blocked+v-linear-blocked"   
+# [7,] "a-linear-blocked+v-linear-blocked"
+# [8,] "a-dExp+v-dExp"  
+  "a-step-fixed+v-step-fixed" = list(
+    a = a_step_fixed,
+    v = v_step_fixed,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = TRUE
+  ),
+  "a-exp+v-exp" = list(
+    a = a_exp,
+    v = v_exp,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-exp+v-linear-blocked" = list(
+    a = a_exp,
+    v = v_linear_blocked,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-exp+v-step-fixed" = list(
+    a = a_exp,
+    v = v_step_fixed,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = TRUE
+  ),
+  "a-dExp-blocked+v-linear-blocked" = list(
+    a = a_dExp_blocked,
+    v = v_linear_blocked,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-exp-blocked+v-linear-blocked" = list(
+    a = a_exp_blocked,
+    v = v_linear_blocked,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-linear-blocked+v-linear-blocked" = list(
+    a = a_linear_blocked,
+    v = v_linear_blocked,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-dExp+v-dExp" = list(
+    a = a_dExp,
+    v = v_dExp,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-linear-blocked+v-linear" = list(
+    a = a_linear_blocked,
+    v = v_linear,
+    z = z_standard,
+    t0 = t0_standard,
+    blocked_likelihood = FALSE
+  ),
+  "a-block-trial-exp+v-exp" = list(
+    a = a_block_trial_exp,
+    v = v_exp,
     z = z_standard,
     t0 = t0_standard,
     blocked_likelihood = TRUE
