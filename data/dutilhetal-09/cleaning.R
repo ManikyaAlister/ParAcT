c1da5d964ac0e70985173b561820c4c3333bd380
ManikyaAlister <alisterm@student.unimelb.edu.au>
2025-02-20 18:21:21 +1100

    updating code for dutilh

--- data/dutilhetal-09/cleaning.R
+++ data/dutilhetal-09/cleaning.R
@@ -5,23 +5,23 @@ library(dplyr)
 d_raw <- read.csv(here("data/dutilhetal-09/raw/gillesdutilh.com_materials_DutilhPB&R2009_practice_data.txt"), sep = " ")
 
 d_clean <- d_raw %>%
-  # only include first session (first 5 blocks) & RTs less than 10 seconds. 
+  # Only include first session (first 5 blocks) & RTs less than 10 seconds  
   filter(block <= 5 & RT < 10000) %>% 
+  group_by(subj) %>% 
   mutate(
-    # rename RT and convert to seconds 
-    Time = RT/1000,
-    # For our code, stim actually needs to be wnw (word/non-word)
+    # Rename RT and convert to seconds  
+    Time = RT / 1000,
+    Trial = row_number(),  # Compute cumulative trial number within each subject  
+    # For our code, stim actually needs to be wnw (word/non-word)  
     Stim = wnw,
-    Resp = correct + 1, # adjust so that 2 is correct, 1 is incorrect
+    Resp = correct + 1,  # Adjust so that 2 is correct, 1 is incorrect  
     Cond = 1,
-    Emphasis = ifelse(substr(subj, 1, 1) == "S", "Speed", "Accuracy" )
-    #subj = sub('^.','',subj)
+    Emphasis = ifelse(substr(subj, 1, 1) == "S", "Speed", "Accuracy")
   ) %>%
   rename(
     "Subj" = subj,
-    "Block" = block,
-    "Trial" = trialnr
-      ) %>%
+    "Block" = block
+  ) %>%
   select(
     Subj,
     Block,
@@ -31,7 +31,10 @@ d_clean <- d_raw %>%
     Stim,
     Cond,
     Emphasis
-  )
+  ) %>%
+  ungroup()  # Ensure calculations don’t persist across groups  
+
+
 
 # Figure out whether their were any bad subject (accuracy < .7)
 subj_accuracy <- d_clean %>%
@@ -49,5 +52,14 @@ if(length(bad_subs > 0)){
 }
 
 # save a different data set for each subject
+subs <- unique(d_clean$Subj)
+n_subs <- length(subs)
 
+for (i in 1:n_subs) {
+  data <- NULL
+  sub <- subs[i]
+  data <- d_clean %>%
+    filter(Subj == sub)
+  save(data, file = here(paste0("data/dutilhetal-09/clean/P",i,".Rdata")))
+}
 
---
+++ modelling/dutilhetal-09/round-1/10_model-comparison-phase-1.R
@@ -0,0 +1,182 @@
+rm(list=ls())
+lib = .libPaths("~/Library/Frameworks/R.framework/Versions/4.1/Resources/library")
+library(here, lib.loc = lib)
+library(modelProb)
+
+n = 4
+# round 1 models 
+
+v_models <- c(
+  "v-exp",
+  "v-dExp",
+  "v-exp-blocked",
+  "v-block-trial-exp",
+  "v-dExp-blocked"
+)
+a_models <- c(
+  "a-exp",
+  "a-dExp",
+  "a-exp-blocked",
+  "a-dExp-blocked",
+  "a-block-trial-exp"
+)
+
+
+models <- c("simple",a_models, v_models)
+
+
+IC_array = function(models, criterion) {
+  # set up empty array
+  allIC <- as.data.frame(matrix(ncol = length(models)))
+  colnames(allIC) = c(models)
+  
+  for (model in models) {
+    for (i in 1:n) {
+      load(here(
+        paste(
+          "data/knowlesetal-19/derived/P",
+          i,
+          "_",
+          model,
+          "-IC.Rdata",
+          sep = ""
+        )
+      ))
+      if (criterion == "AIC") {
+        IC <- AIC
+      } else if (criterion == "BIC") {
+        IC <- BIC
+      }
+      allIC[i, model] = IC
+      
+    }
+  }
+  allIC
+}
+
+allAIC <- IC_array(models,"AIC")
+allBIC <- IC_array(models,"BIC")
+
+#save(allAIC, file = here("data/knowlesetal-19/derived/allAIC.Rdata"))
+#save(allBIC, file = here("data/knowlesetal-19/derived/allBIC.Rdata"))
+
+allAIC_v <- IC_array(v_models, "AIC")
+allAIC_a <- IC_array(a_models, "AIC")
+
+allBIC_v <- IC_array(v_models,"BIC")
+allBIC_a <- IC_array(a_models, "BIC")
+
+rank_models <- function(scores_array) {
+  # apply the ranking function to each row of the array
+  ranked_array <- t(apply(scores_array, 1, rank))
+  
+  # create a new array to store the model names
+  model_names <- array(dim = dim(ranked_array), dimnames = dimnames(ranked_array))
+  
+  # loop over each row of the ranked array
+  for (i in 1:nrow(ranked_array)) {
+    # sort the row by the rank and get the corresponding column names
+    model_names[i,] <- colnames(ranked_array)[order(ranked_array[i,])]
+  }
+  
+  colnames(model_names) = 1:length(scores_array[1,])
+  return(model_names)
+}
+
+# Rank the best models for each participant
+rankBIC <- rank_models(allBIC)
+rankAIC <- rank_models(allAIC)
+
+rankBIC_a <- rank_models(allBIC_a)
+rankBIC_v <- rank_models(allBIC_v)
+
+# Narrow that down to the best two models per participant
+best2_v <- rankBIC_v[,1:2]
+best2_a <- rankBIC_a[,1:2]
+
+best2 <- cbind(best2_a, best2_v)
+
+# Figure out all of the two parameter models to run based on the best 2
+models_2p <- array(dim = c(n, 4))
+
+for (i in 1:length(best2[,1])){
+  model <- c()
+  model[1] <- paste0(best2[i,1],"+",best2[i,3])
+  model[2] <- paste0(best2[i,1],"+",best2[i,4])
+  model[3] <- paste0(best2[i,2],"+",best2[i,4])
+  model[4] <- paste0(best2[i,2],"+",best2[i,3])
+  
+  models_2p[i,] <- model
+}
+
+unique_2p <- unique(models_2p)
+
+# Narrow that down to the best model because best two results in a lot of models
+best_v <- rankBIC_v[,1]
+# save(best_v, file = here("data/knowlesetal-19/derived/best_v_1p_BIC"))
+best_a <- rankBIC_a[,1]
+# save(best_a, file = here("data/knowlesetal-19/derived/best_a_1p_BIC"))
+
+
+best <- cbind(best_a, best_v)
+
+# Figure out all of the two parameter models to run for each participant, based on their best single parameterm models
+models_2p_best <- array(dim = c(n, 1))
+
+for (i in 1:length(best[,1])){
+  models_2p_best
+  models_2p_best[i] <- paste0(best[i,1],"+",best[i,2])
+}
+
+
+count_2p_best <- table(models_2p_best)
+sort(count_2p_best, decreasing = TRUE)
+
+# since there are so many participants, we are only going to take combinations that account for at least 10% of participants (~14)
+unique_2p_best <- names(count_2p_best[count_2p_best > 14]) # naming it this is to keep it consistent with the other data sets. 
+
+save(file = here("data/knowlesetal-19/derived/round-2-models.Rdata"), unique_2p_best)
+
+BIC_weights <- modelProb::weightedICs(allBIC)
+BICmeans <- apply(BIC_weights, 2, mean)
+barplot(BICmeans)
+AIC_weights <- modelProb::weightedICs(allAIC)
+AICmeans <- apply(AIC_weights, 2, mean)
+barplot(AICmeans)
+
+
+modelProb::plotWeightedICs(AIC_weights)
+
+# Best model vesus simple model
+rank_no_simple <- rank_models(dplyr::select(allBIC, -simple) )
+best_models <- rank_no_simple[,1]
+
+best_BICs <- c()
+for ( i in 1:length(allBIC[,1])){
+  best_BICs[i] <- allBIC[i,best_models[i]]
+}
+
+simple = allBIC[,"simple"]
+simpleVbest = cbind(best_BICs, simple)
+weighted_simpleVbest <- weightedICs(simpleVbest)
+modelProb::MMComparisonPlot(weighted_simpleVbest, "simple", "best_BICs", groupNames = c("simple", "best alternative"))
+
+a_exp_models <- models[grep("a-exp", models)]
+non_a_exp_models <- models[!(models %in% a_exp_models)]
+modelProb::MMComparisonPlot(BIC_weights, a_exp_models, non_a_exp_models, groupNames = c("a-exp Models", "Other Models"))
+
+a_dExp_models <- models[grep("a-dExp", models)]
+non_a_dExp_models <- models[!(models %in% a_dExp_models)]
+modelProb::MMComparisonPlot(BIC_weights, a_dExp_models, non_a_dExp_models, groupNames = c("a-delayed exp Models", "Other Models"))
+
+v_exp_models <- models[c(grep("v-exp", models), grep("v-a-exp", models))]
+non_v_exp_models <- models[!(models %in% v_exp_models)]
+modelProb::MMComparisonPlot(BIC_weights, v_exp_models, non_v_exp_models, groupNames = c("v-exp Models", "Other Models"))
+
+v_linear_models <- models[c(grep("v-linear", models))]
+non_v_linear_models <- models[!(models %in% v_linear_models)]
+modelProb::MMComparisonPlot(BIC_weights, v_linear_models, non_v_linear_models, groupNames = c("v-linear Models", "Other Models"))
+
+v_dExp_models <- models[grep("v-dExp", models)]
+non_v_dExp_models <- models[!(models %in% v_dExp_models)]
+modelProb::MMComparisonPlot(BIC_weights, v_dExp_models, non_v_dExp_models, groupNames = c("v-delayed exp Models", "Other Models"))
